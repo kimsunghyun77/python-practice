f74541bca4291fa0424c11b3b50946f509108654
Sunghyun kim <homestay0106@naver.com>
2025-03-30 22:49:36 +0900

    한글 파일 추가

--- 틱택토 게임.py
+++ 틱택토 게임.py
@@ -11,7 +11,7 @@ def check_winner(board, player):
     for col in range(3):
         if all(board[row][col] == player for row in range(3)):
             return True
-    
+        
     if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
         return True
     
